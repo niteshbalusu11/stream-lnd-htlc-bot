2122bc1b50bfb654349e0356f101a8ab69fd532c
Nitesh <niteshbalusu@icloud.com>
2021-11-14 17:27:26 -0500

    2nd commit

--- dist/auth/get_macaroon.js
+++ dist/auth/get_macaroon.js
@@ -1,38 +1,29 @@
 import directory from "./lnd_directory.js";
-import { join } from "path";
-const defaults = [
-    ["bitcoin", "litecoin"],
-    ["mainnet", "testnet"],
-];
-const macDirs = ["data", "chain"];
+import * as dotenv from "dotenv";
+dotenv.config({ path: "../../.env" });
+const environment = process.env.ENVIRONMENT;
+const network = process.env.NETWORK;
+console.log(environment);
+const macDirs = "data/chain";
 const macName = "admin.macaroon";
-import fs from "fs";
-const flatten = (arr) => [].concat(...arr);
+const path = directory();
+console.log(path);
 const getMac = async () => {
-    const [chains, nets] = defaults;
-    let macaroon;
-    let catPath;
-    const path = directory();
-    if (!path) {
-        return new Error("404-UnableToDirectoryPath");
-    }
-    const all = chains.map((chain) => {
-        return nets.map((network) => ({ chain, network }));
-    });
-    const flat = flatten(all);
-    for (var i = 0; i < flat.length; i++) {
-        const macPath = []
-            .concat(macDirs)
-            .concat([flat[i].chain, flat[i].network, macName]);
-        catPath = join(...[path].concat(macPath));
-        if (fs.existsSync(catPath)) {
-            macaroon = fs.readFileSync(catPath, { encoding: "base64" });
-            break;
-        }
-    }
-    if (!macaroon) {
-        throw new Error("404-UnableToDirectoryPath");
-    }
-    return macaroon;
+    // const [chains, nets] = defaults;
+    // let macaroon;
+    // let catPath;
+    // const path = directory();
+    // if (!path) {
+    //   return new Error("404-UnableToDirectoryPath");
+    // }
+    // catPath = join(...[path].concat(macPath));
+    // if (fs.existsSync(catPath)) {
+    //   macaroon = fs.readFileSync(catPath, { encoding: "base64" });
+    //   break;
+    // }
+    // if (!macaroon) {
+    //   throw new Error("404-UnableToDirectoryPath");
+    // }
+    // return macaroon;
 };
 export default getMac;
