78e4af608bf18989ee89190eab03a7bc582ac61f
noderunner11 <niteshbalusu@icloud.com>
2021-11-16 12:43:35 -0500

    fixing write to file conditions

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ sub.on("forward", async (forward) => {
     const response = await constructResponse(forward);
     await sendMessage(response, process.env.CHAT_ID!);
   }
-  if (forward.external_failure !== undefined) {
+  if (!forward.external_failure) {
     writeToFile(forward);
   }
 });
