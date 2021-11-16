6bc2a131e8370ac2b04c1edd9e1967a26f160b9e
noderunner11 <niteshbalusu@icloud.com>
2021-11-16 12:26:25 -0500

    write all forwards

--- dist/index.js
+++ dist/index.js
@@ -12,7 +12,7 @@ sub.on("forward", async (forward) => {
     }
     const response = await constructResponse(forward);
     await sendMessage(response, process.env.CHAT_ID);
-    if (!forward.external_failure) {
-        writeToFile(forward);
-    }
+    // if (!forward.external_failure) {
+    writeToFile(forward);
+    // }
 });
