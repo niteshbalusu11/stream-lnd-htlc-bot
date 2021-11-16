9b79932d15ec76ff75d571734c09dd3a6637e1f7
noderunner11 <niteshbalusu@icloud.com>
2021-11-15 21:45:48 -0500

    log fix

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,11 @@ const botStatus = await startBot();
 console.log(botStatus);
 
 sub.on("forward", async (forward) => {
+  console.log(forward);
   if (!forward || forward.external_failure !== "TEMPORARY_CHANNEL_FAILURE") {
     return;
   }
   const response = await constructResponse(forward);
-  console.log(response);
+  // console.log(response);
   await sendMessage(response, process.env.CHAT_ID!);
 });
