68b9273c90d3ae919a2fd3c90df7fa986bff3ef3
niteshbalusu11 <niteshbalusu@icloud.com>
2021-11-18 11:43:34 -0500

    fixed error message.

--- dist/lnd/walletInfo.js
+++ dist/lnd/walletInfo.js
@@ -2,6 +2,9 @@ import { getWalletInfo } from "lightning";
 import { lnd } from "../auth/authentication.js";
 const walletInfo = async () => {
     const nodePublicKey = (await getWalletInfo({ lnd })).public_key;
+    if (!nodePublicKey) {
+        throw new Error("404-ExpectedYourNodePublicKey");
+    }
     return nodePublicKey;
 };
 export default walletInfo;
--- src/lnd/walletInfo.ts
+++ src/lnd/walletInfo.ts
@@ -4,6 +4,10 @@ import { lnd } from "../auth/authentication.js";
 const walletInfo = async () => {
   const nodePublicKey = (await getWalletInfo({ lnd })).public_key;
 
+  if (!nodePublicKey) {
+    throw new Error("404-ExpectedYourNodePublicKey");
+  }
+
   return nodePublicKey;
 };
 
